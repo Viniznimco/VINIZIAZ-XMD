d3f150463e48110b87e26038aa8342e1a2e3a966
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-31 07:59:09 +0300

    LION🦁

--- viniziaz.js
+++ viniziaz.js
@@ -383,7 +383,7 @@ module.exports = viniziaz = async (_0x38a88a, _0x338562, _0x54bef0, _0x19773a) =
       });
     }
     if (_0x2dea82 && !_0x338562.isGroup) {
-      console.log(chalk.black(chalk.bgWhite("[ RAVEN-BOT ]")), !_0x2437(0x417) ? chalk.green(_0x4bf303) : chalk.keyword("turquoise")(_0x4bf303), chalk.magenta("From"), chalk.green(_0x3d3499), chalk.yellow("[ " + _0x338562.sender.replace('@s.whatsapp.net', '') + " ]"));
+      console.log(chalk.black(chalk.bgWhite("[ RAVEN-BOT ]")), !_0x563d(0x4d5) ? chalk.green(_0x3573be) : chalk.keyword("turquoise")(_0x215155), chalk.magenta("From"), chalk.green(_0x1d8ddf), chalk.yellow("[ " + _0x1d8ddf.sender.replace('@s.whatsapp.net', '') + " ]"));
     } else if (_0x2dea82 && _0x338562.isGroup) {
       console.log(chalk.black(chalk.bgWhite("[ LOGS ]")), !_0x2437(0x417) ? chalk.green(_0x4bf303) : chalk.keyword("turquoise")(_0x4bf303), chalk.magenta("From"), chalk.green(_0x3d3499), chalk.yellow("[ " + _0x338562.sender.replace("@s.whatsapp.net", '') + " ]"), chalk.blueBright('IN'), chalk.green(_0x4444eb));
     }
